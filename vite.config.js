b4405c0baf37b4d9756a356d6f747cbee5c97d1c
Daniel <horokhovbusiness@gmail.com>
2025-06-01 13:52:27 +0200

    chore: add dist

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,35 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 export default defineConfig({
-    root: './',
-    base: './',
-    server: {
-        port: 3000,
-        open: true
+  root: "./",
+  base: "./",
+  server: {
+    port: 3000,
+    open: true,
+  },
+  build: {
+    outDir: "docs",
+    assetsDir: "assets",
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        cardInvitation: resolve(
+          __dirname,
+          "src/components/card-invitation/card-invitation.html"
+        ),
+        createLink: resolve(
+          __dirname,
+          "src/components/create-link/create-link.html"
+        ),
+        linkSentModal: resolve(
+          __dirname,
+          "src/components/link-sent-modal/link-sent-modal.html"
+        ),
+      },
     },
-    build: {
-        outDir: 'dist',
-        assetsDir: 'assets',
-        sourcemap: true
-    },
-    plugins: [
-        tailwindcss()
-    ]
-})
+  },
+  plugins: [tailwindcss()],
+});
